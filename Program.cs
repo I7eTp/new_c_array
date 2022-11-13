b6266d9c3b3c75866433880aecae0440ca2539fa
I7eTp <116294989+I7eTp@users.noreply.github.com>
2022-11-13 20:19:37 +0500

    функциональное програмирование

--- Program.cs
+++ Program.cs
@@ -10,7 +10,7 @@ int a1 = 15;
 int b1 = 21;
 int c1 = 39;
 
-int a2 = 12;
+int a2 = 1200;
 int b2 = 23;
 int c2 = 33;
 
@@ -29,9 +29,12 @@ if(a3 > max) max = a3;
 if(b3 > max) max = b3;
 if(c3 > max) max = c3;
 */
+/*
 int max1 = Max(a1, b1, c1);
 int max2 = Max(a2, b2, c2);
 int max3 = Max(a3, b3, c3);
 int max = Max(max1, max2, max3);
+*/
+int max = Max(Max(a1, b1, c1), Max(a2, b2, c2), Max(a3, b3, c3));
 
 Console.WriteLine(max);
